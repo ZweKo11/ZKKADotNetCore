c250b9479038979aa39713ac5585397c4efc6a93
Zwe Ko <zwekoko12323@gmail.com>
2024-05-09 22:15:27 +0630

    [Complete] LatHtautBayDin Json data to Api

---
+++ ZKKADotNetCore.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
+
+String jsonStr = await File.ReadAllTextAsync("data.json");
+var model = JsonConvert.DeserializeObject<MainDto>(jsonStr);
+
+Console.WriteLine(jsonStr);
+
+//Myanmar number to English number
+//static string ChangeNum(string num)
+//{
+//    num = num.Replace("၁၁", "11");
+//    return num;
+//}
+
+foreach (var num in model.questions)
+{
+    Console.WriteLine(num.questionNo);
+}
+
+Console.ReadLine();
+
+
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
+
