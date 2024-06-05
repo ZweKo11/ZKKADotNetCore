095ecbabab8795ee83100d4e5588e229908f9c35
Zwe Ko <zwekoko12323@gmail.com>
2024-06-05 10:55:30 +0630

    [Complete] NLayer Architecture

---
+++ ZKKADotNetCore.NLayer.DataAcess/Database/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZKKADotNetCore.NLayer.DataAcess;
+using ZKKADotNetCore.NLayer.DataAcess.Database;
+using ZKKADotNetCore.NLayer.DataAcess.Models;
+
+namespace ZKKADotNetCore.NLayer.DataAcess.Database
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ ZKKADotNetCore.NLayer.UI/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ZKKADotNetCore.NLayer.BusinessLogic.Services;
+
+Console.WriteLine("Hello, World!");
+
+BL_Blog bL_Blog = new BL_Blog();
+//bL_Blog.GetBlogs();
